a7c49205b60f14115dc3a5dbcf870d2e02e3c183
FrankOnion <lanaethonis@tuta.io>
2023-11-18 19:56:45 -0500

    new psuedocode

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -4,5 +4,63 @@ public class CashRegister {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Why arent my edits showing?");
+/*		In the cash register we will calculate the amount of change returned to a customer based on the purchase price and the amount tendered.
+	 * 			purpose: output change	
+		 * 			variables: change, purchasePrice, tendered
+		 * 				change: will be calculated (change = tendered - purchase price
+		 * 				purchasePrice: Prompt user
+		 * 				tendered: Prompt user
+ * 
+ *  We will also notify the attendant how many of each piece of currency ($20 ,$10 ,$5 ,$1, .25c, .10c, .05c, .01c) is needed to make the change for the customer. 
+	 *  			Purpose: output pieces of change
+	 	*  			variables: pieces, twenties, tens, fives, ones, quarters, dimes, nickels, pennies
+	 		*			pieces: use a method to count out pieces of change starting at twenties, moving to pennies
+	 		*					denomination = Int(change / value)
+	 		*					change -= change * denomination
+	 		*					if denomination < 0 output " @denomination @currency"
+	 		*					  
+ *  Change should be provided using the largest bill and coin denominations as possible. Denominations that are not used should not be displayed.
+
+		*Hint: Mod operator*
+
+		#### User Story #1
+
+		The user is prompted asking for the price of the item.
+
+		#### User Story #2
+
+		The user is then prompted asking how much money was tendered by the customer.
+
+		#### User Story #3
+
+		Display an appropriate message if the customer provided too little money or the exact amount.
+
+		#### User Story #4
+
+		If the amount tendered is more than the cost of the item, display the number of bills and coins that should be given to the customer.
+
+		### Grading
+
+		This is a graded project.  You are expected to have your project completed by the start of class on Monday morning.  
+
+		You will be given either a pass or fail based on whether your code works given all of the following test conditions:
+
+		* Amount: .67, Tendered: .50, Result: Error message
+		* Amount: .67, Tendered: 1.00, Result: 1 quarter, 1 nickel, 3 pennies.
+		* Amount: .59, Tendered: 1.00, Result: 1 quarter, 1 dime, 1 nickel, 1 penny.
+		* Amount: 3.96, Tendered: 20.00, Result: 1 ten dollar bill, 1 five dollar bill, 1 one dollar bill, 4 pennies.
+		* Amount: any amount less than 20.00, Tendered: anything greater than amount: correct denominations for correct change.
+
+		If the project does work, you will be given a *1* for this week's project.
+
+		If the project does not work by the Monday 8:30 deadlinee, you may be given a 0 with the potential of upgrading to .5 as determined by your instructor.
+
+		To turn in a project, you must push it to a GitHub repository named **MakeChangeProject**.  You must include a `README.md` that describes how to run your program.
+
+		<hr>
+
+		[Up](../README.md)
+*/		
 	}
 }
