07e3e63da3a8503c8f4cea58d8acd35844847e9d
FrankOnion <lanaethonis@tuta.io>
2023-11-18 21:53:35 -0500

    fix 'change to be given:' showing on an insuficient payment

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -13,90 +13,87 @@ public class CashRegister {
 		double price = scanner.nextDouble();
 
 //		  # Prompt clerk for total paid
-		System.out.print("Total paid by customer:");
+		System.out.print("Total paid by customer: ");
 		double tendered = scanner.nextDouble();
 
 //		  # Calculate change change = tendered - price
 		double change = tendered - price;
 
-				  
 //		  # Check if tendered is greater than price 
 		if (change < 0) {
 			System.out.println("Insufficient payment. Please ask for $" + (-1 * change) + " more.");
 			// need to make this show as double point precision
-		}
-		else if (change == 0) {
+		} else if (change == 0) {
 			System.out.println("Exact payment due recieved. No change needed.");
-		}
-		
+		} else {
+
 			// Initialize counter variables
 			int twenties, tens, fives, ones, quarters, dimes, nickels, pennies = 0;
-		
-				  
-		// Helper method to count out currency and update the change
-        // Use integer division to determine the number of each denomination
-        // Update the change accordingly
-        twenties = denominationCounter(change, 20.0);
-        change -= twenties * 20.0;
 
-        tens = denominationCounter(change, 10.0);
-        change -= tens * 10.0;
+			// Helper method to count out currency and update the change
+			// Use integer division to determine the number of each denomination
+			// Update the change accordingly
+			twenties = denominationCounter(change, 20.0);
+			change -= twenties * 20.0;
 
-        fives = denominationCounter(change, 5.0);
-        change -= fives * 5.0;
+			tens = denominationCounter(change, 10.0);
+			change -= tens * 10.0;
 
-        ones = denominationCounter(change, 1.0);
-        change -= ones * 1.0;
+			fives = denominationCounter(change, 5.0);
+			change -= fives * 5.0;
 
-        quarters = denominationCounter(change, 0.25);
-        change -= quarters * 0.25;
+			ones = denominationCounter(change, 1.0);
+			change -= ones * 1.0;
 
-        dimes = denominationCounter(change, 0.1);
-        change -= dimes * 0.1;
+			quarters = denominationCounter(change, 0.25);
+			change -= quarters * 0.25;
 
-        nickels = denominationCounter(change, 0.05);
-        change -= nickels * 0.05;
+			dimes = denominationCounter(change, 0.1);
+			change -= dimes * 0.1;
 
-        pennies = denominationCounter(change, 0.01);
+			nickels = denominationCounter(change, 0.05);
+			change -= nickels * 0.05;
 
-        // Display the change breakdown
-        System.out.println("Change to be given:");
-        if (twenties > 0) {
-            System.out.println(twenties + " twenty-dollar bill(s)");
-        }
-        if (tens > 0) {
-            System.out.println(tens + " ten-dollar bill(s)");
-        }
-        if (fives > 0) {
-            System.out.println(fives + " five-dollar bill(s)");
-        }
-        if (ones > 0) {
-            System.out.println(ones + " one-dollar bill(s)");
-        }
-        if (quarters > 0) {
-            System.out.println(quarters + " quarter(s)");
-        }
-        if (dimes > 0) {
-            System.out.println(dimes + " dime(s)");
-        }
-        if (nickels > 0) {
-            System.out.println(nickels + " nickel(s)");
-        }
-        if (pennies > 0) {
-            System.out.println(pennies + " penny(pennies)");
-        }
-        scanner.close();
-        //I'd like to make output changes pluralization rather than using (s)
+			pennies = denominationCounter(change, 0.01);
 
-    }
+			// Display the change breakdown
+			System.out.println("Change to be given:");
+			if (twenties > 0) {
+				System.out.println(twenties + " twenty-dollar bill(s)");
+			}
+			if (tens > 0) {
+				System.out.println(tens + " ten-dollar bill(s)");
+			}
+			if (fives > 0) {
+				System.out.println(fives + " five-dollar bill(s)");
+			}
+			if (ones > 0) {
+				System.out.println(ones + " one-dollar bill(s)");
+			}
+			if (quarters > 0) {
+				System.out.println(quarters + " quarter(s)");
+			}
+			if (dimes > 0) {
+				System.out.println(dimes + " dime(s)");
+			}
+			if (nickels > 0) {
+				System.out.println(nickels + " nickel(s)");
+			}
+			if (pennies > 0) {
+				System.out.println(pennies + " penny(pennies)");
+			}
+		}
+		// I'd like to make output changes pluralization rather than using (s)
 
-    // Close the scanner
+		// Close the scanner
+		scanner.close();
 
+	}
 
-// Helper method to count out currency and update the change
-public static int denominationCounter(double change, double value) {
-    int count = (int) (change / value);
-    return count;
-}
+	// Helper method to count out currency and update the change
+	public static int denominationCounter(double change, double value) {
+		int count = (int) (change / value);
+		return count;
+	}
 
 }
