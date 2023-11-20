238752008371d75e4795cf159186adf9f4f5ce6f
FrankOnion <lanaethonis@tuta.io>
2023-11-20 10:18:34 -0500

    Reverted code when an issue was found after improvements

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -1,18 +1,13 @@
+// currently failing to give the right number of pennies under at times
+// ex input due: 20 , paid: 20.03 output 3 pennies
+//    input due: 3.96 , paid: 20 output 3 pennies (should be 4)
+//
+// possible calc change based on % opperator.... as a fix
 
 package makechange;
 
 import java.util.Scanner;
 
-/*	Future improvements:
- * Reject invalid user input such as strings
- * 
- *  loop prog for multiple transactions
- *  
- *  allow updates for the amount owed in the event that a customer adds an item after the transaction has begun
- *  allow update to the total paid if the customer adds money after finding they are short on payment
- * 
- * 
- */
 public class CashRegister {
 
 	public static void main(String[] args) {
@@ -29,132 +24,90 @@ public class CashRegister {
 
 //		  # Calculate change change = tendered - price
 		double change = tendered - price;
-		int cents = (int) (change * 100);
 
 //		  # Check if tendered is greater than price 
 		if (change < 0) {
 			change = -change;
-			cents = -cents;
-			if (cents < 100) {
-				System.out.printf("Insufficient payment. Please ask for %d¢ more.", cents);
-			} else {
-				System.out.printf("Insufficient payment. Please ask for $%.2f more.", change);
-			}
+			System.out.printf("Insufficient payment. Please ask for $%.2f more.", change);
 		} else if (change == 0) {
 			System.out.println("Exact payment due recieved. No change needed.");
-		} else if (cents < 100) {
-			System.out.printf("Change to be given: %d¢.", cents);
 		} else {
 			System.out.printf("Change to be given: $%.2f%n", change);
-			// Initialize counter variables and start looking at change in terms of cents to
-			// make calculations easy and exact
-
+			// Initialize counter variables
 			int twenties, tens, fives, ones, quarters, dimes, nickels, pennies = 0;
 
-			// Use denominationCounter() method to count out currency
-			twenties = denominationCounter(cents, 2000);
-			cents -= twenties * 2000;
+			// Helper method to count out currency and update the change
+			// Use integer division to determine the number of each denomination
+			// Update the change accordingly
+			twenties = denominationCounter(change, 20.0);
+			change -= twenties * 20.0;
 
-			tens = denominationCounter(cents, 1000);
-			cents -= tens * 1000;
+			tens = denominationCounter(change, 10.0);
+			change -= tens * 10.0;
 
-			fives = denominationCounter(cents, 500);
-			cents -= fives * 500;
+			fives = denominationCounter(change, 5.0);
+			change -= fives * 5.0;
 
-			ones = denominationCounter(cents, 100);
-			cents -= ones * 100;
+			ones = denominationCounter(change, 1.0);
+			change -= ones * 1.0;
 
-			quarters = denominationCounter(cents, 25);
-			cents -= quarters * 25;
+			quarters = denominationCounter(change, 0.25);
+			change -= quarters * 0.25;
 
-			dimes = denominationCounter(cents, 10);
-			cents -= dimes * 10;
+			dimes = denominationCounter(change, 0.1);
+			change -= dimes * 0.1;
 
-			nickels = denominationCounter(cents, 5);
-			cents -= nickels * 5;
+			nickels = denominationCounter(change, 0.05);
+			change -= nickels * 0.05;
 
-			pennies = denominationCounter(cents, 1);
+			pennies = denominationCounter(change, 0.01);
 
 			// Display the change breakdown
 
 			if (twenties > 0) {
-				System.out.print(twenties + " twenty-dollar bill");
-				if (twenties > 1) {
-					System.out.println("s");
-				} else {
-					System.out.print("\n");
-				}
+				System.out.println(twenties + " twenty-dollar bill(s)");
 			}
-			
-			if (tens> 0) {
-				System.out.print(tens + " ten-dollar bill");
-				if (tens > 1) {
-					System.out.println("s");
-				} else {
-					System.out.print("\n");
-				}
+			if (tens > 0) {
+				System.out.println(tens + " ten-dollar bill(s)");
 			}
-			
 			if (fives > 0) {
-				System.out.print(twenties + " five-dollar bill");
-				if (fives > 1) {
-					System.out.println("s");
-				} else {
-					System.out.print("\n");
-				}
+				System.out.println(fives + " five-dollar bill(s)");
 			}
-			
 			if (ones > 0) {
-				System.out.print(twenties + " one-dollar bill");
-				if (ones > 1) {
-					System.out.println("s");
-				} else {
-					System.out.print("\n");
-				}
+				System.out.println(ones + " one-dollar bill(s)");
 			}
-			
 			if (quarters > 0) {
-				System.out.print(twenties + " quarter");
-				if (quarters > 1) {
-					System.out.println("s");
-				} else {
-					System.out.print("\n");
-				}
+				System.out.println(quarters + " quarter(s)");
 			}
-			
 			if (dimes > 0) {
-				System.out.print(dimes + " dime");
-				if (dimes > 1) {
-					System.out.println("s");
-				} else {
-					System.out.print("\n");
-				}
+				System.out.println(dimes + " dime(s)");
 			}
-			
 			if (nickels > 0) {
-				System.out.print(twenties + " nickel");
-				if (nickels > 1) {
-					System.out.println("s");
-				} else {
-					System.out.print("\n");
-				}
+				System.out.println(nickels + " nickel(s)");
 			}
-			
 			if (pennies > 0) {
-				System.out.print(twenties + " penny");
-				if (twenties > 1) {
-					System.out.println(" pennies");
-				} 
-			}}
+				System.out.println(pennies + " penny(pennies)");
+			}
+		}
+		// ^ I'd like to make output changes pluralization rather than using (s)
+		
+		// ^ Other improvements: loop program until closed, to support multiple
+		// transactions
+		
+		// ^ allow updates for the amount owed in the event that a customer adds an item
+		// after the transaction has begun
+		
+		// ^ allow update to the total paid if the customer adds money after finding
+		// they are short on payment
+
 		// Close the scanner
-		scanner.close();}
+		scanner.close();
 
-	
+	}
 
-	// Helper method to count out currency and update the cents owed
-	public static int denominationCounter(int cents, int value) {
-		int count = (cents / value);
-
+	// Helper method to count out currency and update the change
+	public static int denominationCounter(double change, double value) {
+		int count = (int) (change / value);
 		return count;
 	}
 
